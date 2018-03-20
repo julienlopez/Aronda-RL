f59d578218b89fd87f3cb4927f1592b5fdfb8f2e
julien lopez <julien.lopez51@gmail.com>
2018-03-20 17:31:35 +0100

    trying to make travis-ci work

--- travis-scripts/dependencies.sh
+++ travis-scripts/dependencies.sh
@@ -5,9 +5,6 @@ git clone https://github.com/Microsoft/GSL
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get update
-
-sudo apt-cache search eigen
-
-sudo apt-get install -y cmake libboost-all-dev gcc-7 g++-7 lcov eigen
+sudo apt-get install -y cmake libboost-all-dev gcc-7 g++-7 lcov libeigen3-dev
 
 sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-7 90
